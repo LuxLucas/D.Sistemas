f8f4823bbae873e794e1f2fd11a94a0762e7251b
Lucas Flôres <Luc4sPizoniFlores@gmail.com>
2023-06-13 23:03:03 -0300

    Projeto Beleza_no_dia

---
+++ BelezaNoDia/Units/U_Login.pas
@@ -0,0 +1,43 @@
+unit U_Login;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.Imaging.pngimage,
+  Vcl.ExtCtrls, Vcl.Buttons, Vcl.ColorGrd;
+
+type
+  TForm1 = class(TForm)
+    Edit2: TEdit;
+    Edit1: TEdit;
+    Image1: TImage;
+    Panel1: TPanel;
+    mo: TSpeedButton;
+    procedure moMouseEnter(Sender: TObject);
+    procedure panelTrocaCor(VPanel: TPanel; VCorPanel: TColor);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm1.panelTrocaCor(VPanel: TPanel; VCorPanel: TColor);
+begin
+  VPanel.color := VCorPanel;
+end;
+
+procedure TForm1.moMouseEnter(Sender: TObject);
+begin
+  panelTrocaCor(panel1, $0000C9FF);
+end;
+
+end.
---
+++ BelezaNoDia/unit/U_Login.pas
@@ -0,0 +1,30 @@
+unit U_Login;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.Imaging.pngimage,
+  Vcl.ExtCtrls, Vcl.Buttons;
+
+type
+  TForm1 = class(TForm)
+    Edit2: TEdit;
+    Edit1: TEdit;
+    Image1: TImage;
+    Button1: TButton;
+    BitBtn1: TBitBtn;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+end.
